ea2183ac20753ba8f043f1a0b394d2136f7015a5
David King <davidkingmwai@gmail.com>
2023-05-23 18:25:58 +0300

    Created Project

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Molot: require("./assets/fonts/Molot.otf"),
+    "Metropolis-Regular": require("./assets/fonts/Metropolis-Regular.otf"),
+    "Metropolis-SemiBold": require("./assets/fonts/Metropolis-SemiBold.otf"),
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.landingText}>ROOMIE ROULETTE</Text>
+      <Text style={styles.welcome}>
+        Welcome to Roomie Roulette. You can start here
+      </Text>
+      <View style={{ flexDirection: "row" }}>
+        
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#8454EA",
+    justifyContent: "center",
+  },
+
+  landingText: {
+    fontSize: 70,
+    fontFamily: "Molot",
+    marginLeft: 20,
+    lineHeight: 70,
+    marginBottom: 20,
+  },
+
+  welcome: {
+    fontSize: 20,
+    marginLeft: 20,
+    fontFamily: "Metropolis-SemiBold",
+    color: "#fff",
+    maxWidth: "75%",
   },
 });
