c85c3270d3b8e47b94385c0bca7c38520544bb09
David King <davidkingmwai@gmail.com>
2023-05-24 00:56:17 +0300

    Update

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -1,28 +1,69 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import AppLoading from "expo-app-loading";
 import { useFonts } from "expo-font";
-import { SafeAreaView, TouchableOpacity, Text, StyleSheet } from "react-native";
+import { SafeAreaView, TouchableOpacity, Text, StyleSheet, Animated } from "react-native";
 
 function HomePage(props) {
-    const [fontsLoaded] = useFonts({
-        Molot: require("../assets/fonts/Molot.otf"),
-        "Metropolis-Regular": require("../assets/fonts/Metropolis-Regular.otf"),
-        "Metropolis-SemiBold": require("../assets/fonts/Metropolis-SemiBold.otf"),
-      });
-      if (!fontsLoaded) {
-        return <AppLoading />;
-      }
+  const [isClicked, setIsClicked] = useState(false);
+
+  const springValue = useRef(new Animated.Value(1)).current;
+  const shadowOffsetValue = useRef(new Animated.Value(0)).current;
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+
+    Animated.spring(springValue, {
+      toValue: isClicked ? 1 : 1.2,
+      stiffness: 200,
+      damping: 8,
+      useNativeDriver: false,
+    }).start();
+
+    Animated.spring(shadowOffsetValue, {
+      toValue: isClicked ? 0 : 15,
+      duration: 300,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const [fontsLoaded] = useFonts({
+    Molot: require("../assets/fonts/Molot.otf"),
+    "Metropolis-Regular": require("../assets/fonts/Metropolis-Regular.otf"),
+    "Metropolis-SemiBold": require("../assets/fonts/Metropolis-SemiBold.otf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  const textShadowStyle = {
+    textShadowColor: "#fff",
+    textShadowOffset: { width: shadowOffsetValue, height: shadowOffsetValue },
+    textShadowRadius: 0,
+  };
+
   return (
     <LinearGradient
-      colors={["#54EA90", "#8454EA"]} // Specify the colors you want to use
-      start={{ x: -0.5, y: -0.8 }} // Start from the top left corner
-      end={{ x: 1, y: 1 }} // End at the bottom right corner
-      style={{ flex: 1 }} // Make sure the gradient fills the entire screen
+      colors={["#54EA90", "#8454EA"]}
+      start={{ x: -0.5, y: -0.8 }}
+      end={{ x: 1, y: 1 }}
+      style={{ flex: 1 }}
     >
       <SafeAreaView style={styles.container}>
-        <TouchableOpacity style={styles.roundButton}>
-          <Text style={styles.text}>R</Text>
+        <TouchableOpacity style={styles.roundButton} onPress={handleClick}>
+          <Animated.Text
+            style={[
+              styles.text,
+              {
+                transform: [{ scale: springValue }],
+                ...textShadowStyle,
+              },
+            ]}
+          >
+            R
+          </Animated.Text>
+          <Text style={styles.subText}>oomie</Text>
         </TouchableOpacity>
       </SafeAreaView>
     </LinearGradient>
@@ -33,9 +74,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: 'center',
+    alignItems: "center",
   },
-
   roundButton: {
     width: 350,
     height: 350,
@@ -45,17 +85,16 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-
   text: {
     fontSize: 200,
     fontFamily: "Molot",
-    shadowColor: "#fff",
-    shadowOffset: {
-      width: 10,
-      height: 10,
-    },
-    shadowOpacity: 1,
-    shadowRadius: 0,
+    width: "45%",
+  },
+
+  subText: {
+    fontSize: 30,
+    fontFamily: "Molot",
   },
 });
+
 export default HomePage;
